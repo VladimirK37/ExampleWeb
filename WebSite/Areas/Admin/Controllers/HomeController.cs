88bab7dae7e15b2ec51b789661c840d4b91b902c
VladimirK37 <61294220+VladimirK37@users.noreply.github.com>
2021-02-28 00:51:43 +0300

    Create control panel

--- WebSite/Areas/Admin/Controllers/HomeController.cs
+++ WebSite/Areas/Admin/Controllers/HomeController.cs
@@ -3,15 +3,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebSite.Domain;
 
 namespace WebSite.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class HomeController : Controller
     {
+        private readonly DataManager dataManager;
+
+        public HomeController(DataManager d)
+        {
+            dataManager = d;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(dataManager.ServiceItems.GetServiceItems());
         }
     }
 }
---
+++ WebSite/Areas/Admin/Controllers/TextFieldsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebSite.Domain;
+using WebSite.Domain.Entities;
+using WebSite.Service;
+
+namespace WebSite.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class TextFieldsController : Controller
+    {
+        private readonly DataManager dataManager;
+
+        public TextFieldsController(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
+        public IActionResult Edit(string codeWorod)
+        {
+            var entity = dataManager.TextFields.GetTextFieldByCodeWord(codeWorod);
+            return View(entity);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(TextField model)
+        {
+            if (ModelState.IsValid)
+            {
+                dataManager.TextFields.SaveTextField(model);
+                return RedirectToAction(nameof(HomeController.Index), nameof(HomeController).CutController());
+            }
+            return View(model);
+        }
+
+    }
+}
